0959b880827d4cfecb9c6857f178c8b63d821e94
Enrico Rossetti <hairic95@gmail.com>
2020-10-14 19:25:00 +0200

    fixes

--- src/scenes/TestScene.gd
+++ src/scenes/TestScene.gd
@@ -12,31 +12,28 @@ func _ready():
 	
 	$LevelGeneration.create_level_with_explosion()
 	
+	print($LevelGeneration.tiles)
+	
 	for tile in ($LevelGeneration.tiles):
-		$Statics/TileMap.set_cell(tile.x, tile.y, 0)
+		$Statics.set_cell(tile.x, tile.y, 0)
 	for wall in ($LevelGeneration.walls):
-		$YSortable/StaticsWalls/TileMap.set_cell(wall.x, wall.y, 1)
+		$YSortable/StaticWalls.set_cell(wall.x, wall.y, 1)
 	for outerwall in ($LevelGeneration.outerwalls):
-		$YSortable/StaticsWalls/TileMap.set_cell(outerwall.x, outerwall.y, 2)
+		$YSortable/StaticWalls.set_cell(outerwall.x, outerwall.y, 2)
 
 func add_bullet(bullet_instance, starting_position, bullet_rotation):
 	bullet_instance.global_position = starting_position
 	bullet_instance.rotation = bullet_rotation
 	bullet_instance.setup()
-	$Bullets.add_child(bullet_instance)
+	$YSortable/Bullets.add_child(bullet_instance)
 
 func set_new_interaction(obj : InteractiveObject):
 	var new_label = Label.new()
-	new_label.text = obj.message
-	new_label.rect_global_position = obj.global_position
-	$Popup.add_child(new_label)
 	player.current_interactive_obj = obj
-	$UI/Control/Label.text = obj.message
 
 func close_current_interaction(obj : InteractiveObject):
 	if player.current_interactive_obj == obj:
 		player.current_interactive_obj = null
-		$UI/Control/Label.text = ""
 
 func test_throw_weapon(direction):
 	var new_thrown_weapon = load("res://src/entities/objects/InteractiveWeapon.tscn").instance()
