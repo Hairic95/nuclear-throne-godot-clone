b8f17428ae20ae5439bec664651fcd93f9eb3519
Hairic Lilred <hairic95@gmail.com>
2020-10-16 13:59:22 +0200

    reworked and fixed shotgun

--- src/entities/bullets/Pellet.gd
+++ src/entities/bullets/Pellet.gd
@@ -1,26 +1,21 @@
-extends Area2D
+extends KinematicBody2D
 
 var direction : Vector2 = Vector2.ZERO
 
 export (float) var speed = 200
 
 func _physics_process(delta):
-	global_position += direction * speed  * delta
 	
-	#speed = lerp(speed, 0, 0.05)
+	speed = lerp(speed, 0, 0.1)
 	
-	if speed <= 15:
+	var collision = move_and_collide(direction * speed  * delta)
+	
+	if speed <= 25:
 		queue_free()
 	
-	if $Direction.is_colliding():
-		if !$Direction.get_collider().is_in_group("player"):
-			if $Direction.get_collision_normal() != Vector2.ZERO:
-				direction = direction.bounce($Direction.get_collision_normal().normalized())
-				rotation = direction.angle()
-			else:
-				
-				direction = (global_position - $Direction.get_collision_point()).normalized()
-				rotation = direction.angle()
+	if collision:
+		direction = direction.bounce(collision.normal)
+		rotation = direction.angle()
 
 func setup():
 	direction = Vector2(cos(rotation), sin(rotation))
--- src/entities/weapons/Shotgun.gd
+++ src/entities/weapons/Shotgun.gd
@@ -2,3 +2,15 @@ extends Weapon
 
 func _ready():
 	drop_weapon = load("res://src/entities/objects/dropWeapons/DropShotgun.tscn")
+
+func shoot():
+	
+	$ShootEffect.pitch_scale = 0.7 + (randf() / 2 - 0.25)
+	$ShootEffect.play()
+	for i in range(3):
+		EventBus.emit_signal("create_bullet", bullet_reference.instance(), global_position, rotation + randf() / 12)
+		EventBus.emit_signal("create_bullet", bullet_reference.instance(), global_position, rotation - randf() / 12)
+		
+		yield(get_tree().create_timer(.02), "timeout")
+	EventBus.emit_signal("start_screenshake", 10)
+	
